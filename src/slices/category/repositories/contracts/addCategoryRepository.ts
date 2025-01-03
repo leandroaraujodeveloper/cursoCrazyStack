af8e14a7472513086a3bc156c0030d09100133d4
Leandro <leandroaraujodeveloper@gmail.com>
2025-01-03 08:52:50 -0300

    MODULO 02 AULA 02 - Gerador de teste genérico

---
+++ src/slices/category/repositories/contracts/addCategoryRepository.ts
@@ -0,0 +1,6 @@
+import { CategoryData } from "@/slices/category/entities";
+
+export interface AddCategoryRepository {
+    addCategory(category: CategoryData): Promise<CategoryData | null>;
+    
+}
